74df79fded65cb14503861f8f6965a71652f2148
depo <pangsoradepo12@gmail.com>
2020-03-24 10:01:04 +0700

    "Simple SpringBoot RestApi"

---
+++ src/main/java/com/example/controller/ApiController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+	@RequestMapping("/data")
+	public String Data()
+	{
+		return "Data 1122";
+	}
+}
